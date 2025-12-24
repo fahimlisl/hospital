4d25018f722a865c6ba66c9561af3aefbd178882
fahimlisl <developerfahim134@gmail.com>
2025-12-24 13:29:51 +0530

    debuging unauthorization

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,43 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_BASE_URL,
+//   withCredentials: true, 
+// });
+
+// export default api;
+
+
+
+// debuing process
+
 import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  withCredentials: true, 
 });
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("accessToken");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
